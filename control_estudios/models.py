590f7d11b3728c5ed609188079af8a625e5c8778
santiago nunez <santiagonunez077@gmail.com>
2023-08-05 00:12:45 -0300

    se agrega el proyecto

---
+++ control_estudios/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+class Curso(models.Model):
+    nombre = models.CharField(max_length=64)
+    comision = models.ImageField()
+
+class Estudiante(models.Model):
+    apellido = models.CharField(max_length=256)
+    nombre = models.CharField(max_length=256)
+    email = models.EmailField(blank=True)
+    telefono = models.CharField(max_length=20, blank=True)
+    dni = models.CharField(max_length=32)
+    fecha_nacimiento = models.DateField(null=True)
+
+class Profesor(models.Model):
+    apellido = models.CharField(max_length=256)
+    nombre = models.CharField(max_length=256)
+    dni = models.CharField(max_length=32)
+    email = models.EmailField(blank=True)
+    fecha_nacimiento = models.DateField(null=True, blank=True)
+    profesion = models.CharField(max_length=128)
+    bio = models.TextField(blank=True)
+
+class Entregable(models.Model):
+    nombre = models.CharField(max_length=256)
+    fecha_de_entrega = models.DateField(auto_now_add=True)
+    esta_aprobado = models.BooleanField(default=False)
